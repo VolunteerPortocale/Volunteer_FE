e222b3338d05fd799689101d8a1dd08aa0139cdb
nici40 <ghoulfromtokyoghoul@gmail.com>
2026-09-22 16:43:18 +0300

    VOL-0041: add keycloak-angular and env config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,9 @@ export const environment = {
   envName: 'Local (dev)',
   apiUrl: 'https://volunteer-be-rs60.onrender.com/api',
   graphqlUrl: 'https://volunteer-be-rs60.onrender.com/graphql',
+  backendOrigin: 'https://volunteer-be-rs60.onrender.com',
+  keycloakUrl: 'https://volunteer-kc.duckdns.org',
+  keycloakRealm: 'volunteer',
+  keycloakClientId: 'volunteer-fe',
   basicAuth: 'YWRtaW46cGFzc3dvcmQxMjM='
 };
